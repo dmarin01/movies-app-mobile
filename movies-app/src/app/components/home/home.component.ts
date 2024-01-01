e4bb8aa1800ae6ca74890310f8dc2454b2412478
dmarindev <daniel.marin.depablo@gmail.com>
2024-01-01 20:20:06 +0100

    first components created

---
+++ movies-app/src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
